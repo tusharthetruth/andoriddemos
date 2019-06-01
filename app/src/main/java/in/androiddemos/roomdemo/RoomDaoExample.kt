74b03791d30fc2e1673f5a1d44d8cf77bc285ba4
tushar nigam <tusharnigam88@gmail.com>
2019-06-01 16:33:53 +0530

    dao added

---
+++ app/src/main/java/in/androiddemos/roomdemo/RoomDaoExample.kt
@@ -0,0 +1,13 @@
+package `in`.androiddemos.roomdemo
+
+import `in`.androiddemos.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomDaoExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_dao_example)
+    }
+}
